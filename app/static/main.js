a72e8fa7c73266302976b17b43cb5194ec62bcdf
Kevin Gao <k_gao@aol.com>
2019-04-22 23:44:52 +0200

    css, hamburger menu, code refactoring

--- app/static/main.js
+++ app/static/main.js
@@ -1,38 +1,17 @@
-// make sure the DOM loads first
+// Ensure the DOM is loaded first
 document.addEventListener("DOMContentLoaded", function() {
 
+  // replace textarea with CKEDITOR
+  CKEDITOR.replace("body")
 
-  function start(){
-
-  window.timerID =  setInterval(function() {
-  var aOpaque = document.getElementById('imageID').style.opacity;
-  aOpaque = aOpaque-.1;
-
-  aOpaque = aOpaque.toFixed(1);
-
-  document.getElementById('imageID').style.opacity = aOpaque;
-
-  if(document.getElementById('imageID').style.opacity<=0)
-  clearInterval(window.timerID);
-  },1000);
+  // responsive
+  function myFunction() {
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
   }
 
-  window.onload = function(){start();}
-
-//   // CKEDITOR.replace('body')
-//
-//   // Not needed for now
-//   var navContainer = document.getElementById("navUl");
-//   var links = navContainer.getElementsByClassName("link")
-//
-//   for(var i=0; i<links.length; i++) {
-//     links[i].addEventListener("click", function() {
-//       var current = document.getElementsByClassName("active");
-//       current[0].className = current[0].className.replace(" active", "");
-//       this.className += " active";
-//     });
-//   }
-//
-//   // TODO:
-//   // flash animations!!!
 });
